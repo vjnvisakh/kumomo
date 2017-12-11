b458add969a02067f33fdbfd69a2276f772cde09
Visakh Vijayan <visakh.vawsum@gmail.com>
2017-12-12 02:30:02 +0530

    changes to categories
    Please merge this to master

--- app/Controller/CategoriesController.php
+++ app/Controller/CategoriesController.php
@@ -4,6 +4,7 @@
 
 	class CategoriesController extends AppController
 	{		
+		public $name = "Categories";
 		public $uses = array("Category","CommentsToArticle","SiteHit"); 		
 
 		public function isAlive()
@@ -15,9 +16,39 @@
 		public function index($subCategoryId)
 		{
 			$this -> log("CategoriesController -> index() -> START:".microtime(true),LOG_DEBUG);
+			$this -> layout = "";
 			
-			
+			// If the id is empty then redirect to the home page
+			if(empty($subCategoryId))
+			{
+				$this -> redirect
+				(
+					array
+					(
+						"controller" => "homes",
+						"action" => "index"
+					)
+				);
+			}
+			else
+			{	
+				// This is to fetch the articles for the subcategory
+				$sql  = "";
+				$sql .= " select a.* from articles_to_categories atc inner join articles a on atc.article_id = a.id and a.status = 'active'";
+				$sql .= " and atc.category_id = $subCategoryId";
 
+				$result = $this -> Category -> query($sql);
+
+				// Send the result to the view file
+				$this -> set("articles",$result);
+
+				// This is to fetch the ads
+				$sql = " SELECT * FROM ads WHERE status = 'active' AND photo <> ''";
+				$result = $this -> Category -> query($sql);
+
+				$this -> set("ads",$result);
+			}
+			
 			$this -> log("CategoriesController -> index() -> END:".microtime(true),LOG_DEBUG);			
 		}
 
--- app/View/Elements/header.ctp
+++ app/View/Elements/header.ctp
@@ -1,17 +1,17 @@
 <div class="row" style="padding:0px">
 	<div class="col-lg-6" style="padding:0px"></div>
 	<div class="col-lg-6" style="color:#aaa;text-align:right">
-		<span style="font-size:50%">Updated: <?= date("d-M-Y",strtotime($LAST_UPDATED_ON));?></span>
+		<!-- <span style="font-size:50%">Updated: <?= date("d-M-Y",strtotime($LAST_UPDATED_ON));?></span> -->
 	</div>
 	
 </div>
 
 <div class="row" style="padding:1%">
 	<div class="col-lg-1">
-		<img height="70" width="70" src="<?=$this->webroot.'/images/logo.png'?>">
+		<img height="55" width="55" src="<?=$this->webroot.'/images/logo.png'?>">
 	</div>
-	<div class="col-lg-11" style="color:#fff;margin-left:-1%">
-		<h2>Khashkhobar.in</h2>	
-		<p>Your news stop</p>
+	<div class="col-lg-11" style="color:#fff;margin-left:-2%">
+		<a href="<?=$this->webroot?>"><h3>Khashkhobar.in</h3></a>
+		<p style="margin-top:-1%">Your news stop</p>
 	</div>
 </div>
